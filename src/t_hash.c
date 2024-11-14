6eebc1805e828ab4fa820a15bbbe3148a53cd0e3
Victor <weicheng.king@qq.com>
2024-11-14 17:47:57 +0800

    fix redis hash

--- src/t_hash.c
+++ src/t_hash.c
@@ -482,6 +482,8 @@ void hashTypeConvertZiplist(robj *o, int enc) {
             value = hashTypeCurrentObjectNewSds(hi,OBJ_HASH_VALUE);
             ret = dictAdd(dict, key, value);
             if (ret != DICT_OK) {
+                sdsfree(key);
+                sdsfree(value);
                 serverLogHexDump(LL_WARNING,"ziplist with dup elements dump",
                     o->ptr,ziplistBlobLen(o->ptr));
                 serverPanic("Ziplist corruption detected");
