7812e9068d5db08f74e3533347798546da081b38
Victor <weicheng.king@qq.com>
2024-11-22 09:27:28 +0800

    about meta

--- src/ctrip_swap.h
+++ src/ctrip_swap.h
@@ -521,11 +521,11 @@ typedef struct objectMetaType {
 
 typedef struct objectMeta {
   uint64_t version;
+  int swap_type:4;
   union {
-    long long len;
-    unsigned long long ptr;
+    long long len:60;
+    unsigned long long ptr:60;
   };
-  int swap_type;
 } objectMeta;
 
 extern objectMetaType lenObjectMetaType;
