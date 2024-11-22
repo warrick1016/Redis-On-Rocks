5b859878b6aaad7c2c3486d5314f438fe05637d4
Victor <weicheng.king@qq.com>
2024-11-22 08:49:26 +0800

    epoll

--- src/ae_epoll.c
+++ src/ae_epoll.c
@@ -106,6 +106,7 @@ static void aeApiDelEvent(aeEventLoop *eventLoop, int fd, int delmask) {
     }
 }
 
+REDIS_NO_SANITIZE("address")
 static int aeApiPoll(aeEventLoop *eventLoop, struct timeval *tvp) {
     aeApiState *state = eventLoop->apidata;
     int retval, numevents = 0;
